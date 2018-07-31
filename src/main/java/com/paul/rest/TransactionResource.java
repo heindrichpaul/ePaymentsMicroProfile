e61f52d6af118c0424667f9eff3def459602221a
Heindrich Paul <heindrich.paul@gmail.com>
2018-07-31 19:30:03 +0200

    changed the endpoint so that the verb is on the noun

--- src/main/java/com/paul/rest/TransactionResource.java
+++ src/main/java/com/paul/rest/TransactionResource.java
@@ -10,7 +10,7 @@ import javax.ws.rs.core.Response;
 
 import static com.paul.rest.RestApplication.accounts;
 
-@Path("transfer")
+@Path("accounts/transfer")
 public class TransactionResource {
 
     @POST
